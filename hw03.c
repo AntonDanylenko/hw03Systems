6ed32503ca3dfbdc9fef78bad0ed9f80a628d0df
AntonDanylenko <adanylenko@stuy.edu>
2018-09-19 20:12:54 -0400

    populated second array using only pointers, with the reverse elements of array 1

--- hw03.c
+++ hw03.c
@@ -9,8 +9,19 @@ int main(){
     ary[x] = rand();
   }
   ary[9] = 0;
-  printf("Values in the array:\n");
+  printf("Values in the first array:\n");
   for(int x=0; x<10; x++){
     printf("%d: %d\n", x, ary[x]);
+  }
+
+  int ary1 [10];
+  int *aryp = &ary;
+  int *ary1p = &ary1;
+  for (int x=0; x<10; x++){
+    *(ary1p+x) = *(aryp + 9 - x);
+  }
+  printf("Values in the second array:\n");
+  for(int x=0; x<10; x++){
+    printf("%d: %d\n", x, ary1[x]);
   }
 }
